4a1426254f30ec75ddbc652500e8ebfb9821faa4
Bruno Viana <brunoviana@gmail.com>
2023-12-28 20:34:34 +0000

    adding auth0

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,9 @@ Route::middleware([
         return Inertia::render('Dashboard');
     })->name('dashboard');
 });
+
+
+
+Route::get('/private', function () {
+    return response('Welcome! You are logged in.');
+})->middleware('auth');
