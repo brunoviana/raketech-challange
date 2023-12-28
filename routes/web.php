c9247df4cea074828587e512176512757e010720
Bruno Viana <brunoviana@gmail.com>
2023-12-28 22:19:00 +0000

    auth0 working

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,27 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/private', function () {
+    return response('Welcome! You are logged in.');
+})->middleware('auth');
+
+Route::get('/user', function () {
+    if (! auth()->check()) {
+        return response('You are not logged in.');
+    }
+
+    $user = auth()->user();
+    $name = $user->name ?? 'User';
+    $email = $user->email ?? '';
+
+    return response("Hello {$name}! Your email address is {$email}.");
 });
